ad699f437169b1f28f6913ae07bd40d2cd649afd
pochodnicky <pochodnicky@gratex.com>
2023-08-14 16:22:39 +0200

    ActorMessage handler

--- src/main/scala/org/qwActor/ActorMessage.scala
+++ src/main/scala/org/qwActor/ActorMessage.scala
@@ -6,6 +6,10 @@ object ActorMessage{
   def apply(sender:ActorRef, value:Any, handler:Option[Consumer[Any]] = None) = new ActorMessage(sender, value)
 
   def unapply(m: ActorMessage): Some[(ActorRef, Any, Option[Consumer[Any]])] = Some((m.sender, m.value, m.handler))
+
+  val nil: Consumer[Any] = m => {
+    if (logger.isWarnEnabled) logger.warn("ignored message: " + m)
+  }
 }
 
 class ActorMessage(val sender:ActorRef, val value:Any, val handler:Option[Consumer[Any]] = None){
