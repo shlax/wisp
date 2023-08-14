ecda522afa5f9dab96af84f434fc749300a83bd5
pochodnicky <pochodnicky@gratex.com>
2023-08-14 16:20:15 +0200

    ActorMessage handler

--- src/main/scala/org/qwActor/ActorMessage.scala
+++ src/main/scala/org/qwActor/ActorMessage.scala
@@ -5,7 +5,7 @@ import java.util.function.Consumer
 object ActorMessage{
   def apply(sender:ActorRef, value:Any, handler:Option[Consumer[Any]] = None) = new ActorMessage(sender, value)
 
-  def unapply(m: ActorMessage): Some[(ActorRef, Any)] = Some((m.sender, m.value))
+  def unapply(m: ActorMessage): Some[(ActorRef, Any, Option[Consumer[Any]])] = Some((m.sender, m.value, m.handler))
 }
 
 class ActorMessage(val sender:ActorRef, val value:Any, val handler:Option[Consumer[Any]] = None){
