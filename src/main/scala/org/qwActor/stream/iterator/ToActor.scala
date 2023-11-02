abf0dacae2ec75d4e792d330d7f5450b2ff0490a
pochodnicky <pochodnicky@gratex.com>
2023-11-02 15:34:49 +0100

    ToActor

---
+++ src/main/scala/org/qwActor/stream/iterator/ToActor.scala
@@ -0,0 +1,13 @@
+package org.qwActor.stream.iterator
+
+import org.qwActor.{Actor, ActorContext, ActorMessage, ActorRef}
+
+object ToActor{
+
+  def toActor(ref:ActorRef, context: ActorContext): Actor = new Actor(context){
+    override def process(sender: ActorRef): PartialFunction[Any, Unit] = {
+      case v => ref.accept(new ActorMessage(sender, v))
+    }
+  }
+
+}
