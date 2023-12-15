6f9da85cf8b67e441496789c71dfff51619642d0
pochodnicky <pochodnicky@gratex.com>
2023-12-15 11:19:52 +0100

    Cluster

---
+++ src/main/scala/org/wisp/remote/cluster/topology/Cluster.scala
@@ -0,0 +1,30 @@
+package org.wisp.remote.cluster.topology
+
+import org.wisp.ActorSystem
+import org.wisp.remote.ObjectId
+import org.wisp.remote.cluster.{ClusterEventListener, ClusterSystem}
+
+import java.net.InetSocketAddress
+import java.util.concurrent.CompletableFuture
+
+object Cluster{
+
+  def apply():Cluster = new Cluster()
+
+}
+
+class Cluster extends ClusterEventListener{
+
+  protected def createClusterSystem():ClusterSystem = ClusterSystem(Some(this))
+  val system:ClusterSystem = createClusterSystem()
+
+  def bind(adr:InetSocketAddress):Unit = {
+    system.bind(adr)
+  }
+
+  def connect(self:InetSocketAddress, nodes:Seq[InetSocketAddress]):List[CompletableFuture[ObjectId]] = {
+    val c = ConnectionBalancer.apply(nodes)
+    for(i <- c if i.from == self) yield system.addNode(i.to)
+  }
+  
+}
