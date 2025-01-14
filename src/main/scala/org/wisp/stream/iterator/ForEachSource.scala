4ff665715d3f2a056346a857a0df63e657bc4e1d
pochodnicky <pochodnicky@gratex.com>
2025-01-14 15:04:57 +0100

    ForEachSource

---
+++ src/main/scala/org/wisp/stream/iterator/ForEachSource.scala
@@ -0,0 +1,68 @@
+package org.wisp.stream.iterator
+
+import org.wisp.stream.Source
+import org.wisp.{ActorRef, ActorSystem, Message}
+
+import java.util
+import java.util.concurrent.locks.ReentrantLock
+
+class ForEachSource(it:Source[T], system:ActorSystem) extends ActorRef(system), Runnable {
+
+  private val nodes:util.Queue[ActorRef] = createNodes()
+
+  protected def createNodes(): util.Queue[ActorRef] = {
+    util.LinkedList[ActorRef]()
+  }
+
+  private val lock = new ReentrantLock()
+  private val condition = lock.newCondition()
+
+  private var ended = false
+
+  override def run():Unit = {
+    it.forEach { e =>
+      lock.lock()
+      try {
+        var a = nodes.poll()
+        while (a == null){
+          condition.await()
+          a = nodes.poll()
+        }
+        a << Next(e)
+      } finally {
+        lock.unlock()
+      }
+    }
+
+    lock.lock()
+    try {
+      ended = true
+      var a = nodes.poll()
+      while (a != null) {
+        a << End
+        a = nodes.poll()
+      }
+    } finally {
+      lock.unlock()
+    }
+  }
+
+  override def accept(t: Message): Unit = {
+    t.message match {
+      case HasNext =>
+        lock.lock()
+        try {
+          if (ended) {
+            t.sender << End
+          } else {
+            nodes.add(t.sender)
+            condition.signal()
+          }
+        } finally {
+          lock.unlock()
+        }
+
+    }
+  }
+
+}
