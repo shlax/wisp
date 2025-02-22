3fffa72dfca3505351f616176f458c0e9c527f8a
peter o <slax.10101@gmail.com>
2025-02-22 07:21:39 +0100

    lock

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,6 @@ dependencies {
     implementation("org.scala-lang:scala3-library_3:3.6.3")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.4")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
 
 }
