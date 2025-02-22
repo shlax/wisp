1f770f6f230e382ce9b08c87398249eb9217f245
peter o <slax.10101@gmail.com>
2025-02-22 07:45:54 +0100

    fix gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,10 @@ repositories {
 dependencies {
     implementation("org.scala-lang:scala3-library_3:3.6.3")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.4")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
 
+tasks.test{
+    useJUnitPlatform()
 }
