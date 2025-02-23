f7cc7b5adb1ca1df44a0fee8e16bc91d1f714705
peter o <slax.10101@gmail.com>
2025-02-23 18:57:44 +0100

    Stream forEach

--- src/main/scala/org/wisp/stream/typed/Stream.scala
+++ src/main/scala/org/wisp/stream/typed/Stream.scala
@@ -2,12 +2,21 @@ package org.wisp.stream.typed
 
 import org.wisp.ActorSystem
 import org.wisp.stream.Source
-import org.wisp.stream.iterator.ActorSource
+import org.wisp.stream.iterator.{ActorSource, ForEachSource}
+
+import java.util.function.Consumer
 
 class Stream(val system:ActorSystem){
 
   def from[T](s:Source[T]) : Node[T] = {
-    new Node[T](this, ActorSource(s))
+    Node[T](this, ActorSource(s))
+  }
+
+  def forEach[T, V >: Node[ ? >: T]](s:Source[T])(fn : Consumer[V]) : ForEachSource = {
+    val f = ForEachSource(s)
+    val n = Node[T](this, f)
+    fn.accept(n)
+    f
   }
 
 }
