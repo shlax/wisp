c7dfad6bf3fc306c6d77c7344f9daee0eac66b0b
qnx <az4789317614839174893@gmail.com>
2025-03-01 17:50:38 +0100

    HelloTests 1

--- src/test/scala/org/wisp/test/impl/HelloTests.scala
+++ src/test/scala/org/wisp/test/impl/HelloTests.scala
@@ -1,11 +1,16 @@
 package org.wisp.test.impl
 
 import org.junit.jupiter.api.{Assertions, Test}
+import org.wisp.stream.{Sink, SinkTree}
+import org.wisp.stream.Source.*
 import org.wisp.using.*
 import org.wisp.{Actor, ActorLink, ActorSystem, Inbox}
 
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicReference
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.Promise
+import scala.util.Success
 
 class HelloTests {
 
@@ -55,6 +60,52 @@ class HelloTests {
     })
 
     Assertions.assertEquals("Hello world", ref.get())
+  }
+
+  @Test
+  def helloSinkFlatMap():Unit = {
+    val data = Seq(List(0,1,2),List(3,4,5)).asSource
+    val l = ArrayBuffer[Int]()
+
+    SinkTree(data){ f =>
+      f.flatMap{ (x:Sink[Int]) =>
+        (t: List[Int]) => for (i <- t) x.accept(i)
+      }.map(i => l += i)
+    }
+
+    Assertions.assertEquals(0 to 5, l)
+  }
+
+  @Test
+  def helloSinkFold(): Unit = {
+    val data = Seq(1, 2, 3).asSource
+
+    val p:Promise[Int] = SinkTree(data){ f =>
+      val tmp = f.fold(0)( (a, b) => a + b)
+      Assertions.assertFalse(tmp.isCompleted)
+      tmp
+    }
+
+    Assertions.assertEquals(Some(Success(6)), p.future.value)
+  }
+
+  @Test
+  def helloSourceFlatMap():Unit = {
+    val data = Seq(List(0, 1, 2), List(3, 4, 5)).asSource
+    val l = ArrayBuffer[Int]()
+
+    data.flatMap( i => i.asSource ).forEach(i => l += i)
+
+    Assertions.assertEquals(0 to 5, l)
+  }
+
+  @Test
+  def helloSourceFold(): Unit = {
+    val data = Seq(1, 2, 3).asSource
+
+    val r = data.fold(0)((a, b) => a + b)
+
+    Assertions.assertEquals(6, r)
   }
 
 }
