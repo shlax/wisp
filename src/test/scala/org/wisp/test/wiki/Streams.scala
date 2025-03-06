41e4e70087c97ab3fdfb3b4e3c120226edca02bb
pochodnicky <pochodnicky@gratex.com>
2025-03-06 11:35:35 +0100

    doc

--- src/test/scala/org/wisp/test/wiki/Streams.scala
+++ src/test/scala/org/wisp/test/wiki/Streams.scala
@@ -15,27 +15,20 @@ class Streams {
   def simpleStream():Unit = {
     // create ActorSystem
     ActorSystem() | { system =>
-
       // test data
       val data = 1 to 3
-
       // give system as ExecutionContext
       system.as { sys =>
-        // stream graph builder
+        // create graph builder
         val graph = StreamGraph(sys)
-
         // convert data to Source
         val source = data.asSource
-
         // create stream from source then add 1 and print result
         val stream = graph.from(source).map(_ + 1).to(println)
-
         // start execution and wait for completion
         Await.ready(stream.start(), 1.second)
       }
-
     }
-
   }
 
 }
