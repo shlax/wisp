687a3e7dc91eaeea65f97e199e5a13f5570c7e09
peter o <slax.10101@gmail.com>
2025-03-06 18:48:13 +0100

    .

--- src/main/scala/org/wisp/stream/typed/StreamNode.scala
+++ src/main/scala/org/wisp/stream/typed/StreamNode.scala
@@ -9,17 +9,17 @@ import scala.concurrent.ExecutionContext
 class StreamNode[T](graph: StreamGraph, val link: ActorLink) extends StreamGraph(graph.system){
   
   def map[V](fn: T => V): StreamNode[V] = {
-    val r = graph.system.create( i => StreamWorker.map(link, i, fn) )
+    val r = system.create( i => StreamWorker.map(link, i, fn) )
     graph.node(r)
   }
 
   def filter(fn: T => Boolean): StreamNode[T] = {
-    val r = graph.system.create(i => StreamWorker.filter(link, i, fn))
+    val r = system.create(i => StreamWorker.filter(link, i, fn))
     graph.node(r)
   }
   
   def flatMap[V](fn: T => Source[V]): StreamNode[V] = {
-    val r = graph.system.create(i => StreamWorker.flatMap(link, i, fn) )
+    val r = system.create(i => StreamWorker.flatMap(link, i, fn) )
     graph.node(r)
   }
 
