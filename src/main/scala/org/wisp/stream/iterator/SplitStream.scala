3e69674e35bc1e9acc9b9369ab54820f6d2e0649
pochodnicky <pochodnicky@gratex.com>
2025-03-07 09:00:18 +0100

    SplitStream

--- src/main/scala/org/wisp/stream/iterator/SplitStream.scala
+++ src/main/scala/org/wisp/stream/iterator/SplitStream.scala
@@ -1,32 +1,100 @@
 package org.wisp.stream.iterator
 
 import org.wisp.{ActorLink, Message}
+import org.wisp.lock.*
+import org.wisp.stream.iterator.message.{HasNext, Next, End}
 
+import java.util
+import java.util.concurrent.locks.ReentrantLock
 import scala.concurrent.ExecutionContext
 
-class SplitStream(prev:ActorLink)(link: SplitStream#Split => Unit)(using executor: ExecutionContext) {
+class SplitStream(prev:ActorLink)(link: SplitStream#Split => Unit)(using executor: ExecutionContext) extends StreamException {
 
-  class Split {
-    protected[iterator] var to:List[SplitActorLink] = Nil
+  trait Split {
+    def nextTo(): ActorLink
+  }
 
-    def next : SplitActorLink = {
+  protected class SplitBuilder extends Split {
+    var links:List[SplitActorLink] = Nil
+
+    override def nextTo(): SplitActorLink = {
       val link = SplitActorLink()
-      to = link :: to
+      links = link :: links
       link
     }
 
   }
 
-  class SplitActorLink extends ActorLink{
-    override def accept(t: Message): Unit = ???
+  protected def createNodes(): util.Queue[ActorLink] = {
+    util.LinkedList[ActorLink]()
   }
 
-  protected val next:List[SplitActorLink] = {
-    val s = Split()
+  protected val lock = new ReentrantLock()
+  protected var requested:Boolean = false
+  protected var exception:Option[Throwable] = None
+  protected var ended = false
+
+  protected val next: List[SplitActorLink] = {
+    val s = SplitBuilder()
     link.apply(s)
-    s.to.reverse
+    s.links
   }
 
+  override def accept(t:Message):Unit = lock.withLock {
+    if(!requested) throw new IllegalStateException("not requested")
+    requested = false
 
+    t.value match {
+      case Next(v) =>
+        for(n <- next) n.next(v)
+        pullNext()
+      case End(v) =>
+        if(v.isDefined){
+          exception = v
+        }else{
+          ended = true
+        }
+
+        for(n <- next) n.end()
+    }
+  }
+
+  protected def pullNext():Unit = {
+    if(!requested && next.forall(i => !i.nodes.isEmpty)){
+      requested = true
+      prev.call(HasNext).onComplete(SplitStream.this.accept)
+    }
+  }
+
+  protected class SplitActorLink extends ActorLink{
+    val nodes: util.Queue[ActorLink] = createNodes()
+
+    def next(v:Any): Unit = {
+      val n = nodes.poll()
+      if(n == null) throw new IllegalStateException("nodes are empty")
+      n << Next(v)
+    }
+
+    def end(): Unit = {
+      var n = nodes.poll()
+      if(n == null) throw new IllegalStateException("nodes are empty")
+      while(n != null){
+        n << End(exception)
+        n = nodes.poll()
+      }
+    }
+
+    override def accept(t: Message): Unit = lock.withLock {
+      t.value match {
+        case HasNext =>
+          if(exception.isDefined || ended){
+            t.sender << End(exception)
+          }else{
+            nodes.add(t.sender)
+            pullNext()
+          }
+      }
+    }
+  }
 
 }
