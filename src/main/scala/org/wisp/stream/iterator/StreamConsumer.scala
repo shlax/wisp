020d5cf23ad4a287c3dcb94df17ea696dbd1cd14
pochodnicky <pochodnicky@gratex.com>
2025-04-02 14:29:35 +0200

    doc

--- src/main/scala/org/wisp/stream/iterator/StreamConsumer.scala
+++ src/main/scala/org/wisp/stream/iterator/StreamConsumer.scala
@@ -8,6 +8,10 @@ import scala.util.{Failure, Success, Try}
 
 trait StreamConsumer extends Consumer[Message]{
 
+  /** Convert [[Try]] to stream [[Consumer.accept]].
+   * [[Failure]] will be mapped to [[End]] with `exception`
+   *
+   * Example: `[ActorLink].call(HasNext).onComplete(accept)` */
   def accept(t: Try[Message]): Unit = {
     t match{
       case Success(v) =>
