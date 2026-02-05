b6560d0ad1436a467cfb5cdea56c6f41cb41bf79
pochodnicky <pochodnicky@gratex.com>
2026-02-05 10:32:35 +0100

    update dependencies (scala 3.8.1, junit 6.0.2, h2 2.4.240)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.scala-lang:scala3-library_3:3.7.0")
+    implementation("org.scala-lang:scala3-library_3:3.8.1")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.0.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    testImplementation("com.h2database:h2:2.3.232")
+    testImplementation("com.h2database:h2:2.4.240")
 }
 
 tasks.test{
