74e1361512dc2af669fd8344ebd8f553857eda14
pochodnicky <pochodnicky@gratex.com>
2026-02-27 13:56:10 +0100

    add start method to RunnableStream for running with ExecutionContext

--- src/main/scala/org/wisp/stream/iterator/RunnableStream.scala
+++ src/main/scala/org/wisp/stream/iterator/RunnableStream.scala
@@ -1,3 +1,13 @@
 package org.wisp.stream.iterator
 
-trait RunnableStream extends StreamConsumer, Runnable
+import scala.concurrent.{ExecutionContext, Future}
+
+trait RunnableStream extends StreamConsumer, Runnable {
+
+  def start(using ExecutionContext): Future[Unit] = {
+    Future {
+      run()
+    }
+  }
+
+}
