9005b14b8d906d645bd8ce78a0601428e01c36c0
peter o <slax.10101@gmail.com>
2026-03-02 21:10:39 +0100

    migrate `closeable` and `lock` utilities to `org.wisp.utils`, add `FromMap` type class for object mapping, and update imports across modules

--- src/main/scala/org/wisp/utils/FromMap.scala
+++ src/main/scala/org/wisp/utils/FromMap.scala
@@ -12,23 +12,18 @@ trait FromMap[T] {
 object FromMap {
 
   inline given derived[T](using m: Mirror.Of[T]): FromMap[T] = {
-    val labels = constValueTuple[m.MirroredElemLabels]
+    val labels = constValueTuple[m.MirroredElemLabels].toArray.map(_.toString)
     inline m match
       case p: Mirror.ProductOf[T] => fromMapProduct(p, labels)
   }
 
-  private def fromMapProduct[T](p: Mirror.ProductOf[T], labels:Tuple): FromMap[T] = new FromMap[T] {
-
-    override def fromMap(map:Map[String, ?]):T = {
-      var t: Tuple = EmptyTuple
-
-      for(i <- 0 until labels.productArity){
-        val v = map(labels.productElement(i).toString)
-        t = t :* v
-      }
-      p.fromProduct(t)
+  private def fromMapProduct[T](p: Mirror.ProductOf[T], labels:Array[String]): FromMap[T] = (map: Map[String, ?]) => {
+    var t: Tuple = EmptyTuple
+    for (i <- labels) {
+      val v = map(i)
+      t = t :* v
     }
-
+    p.fromProduct(t)
   }
 
 }
