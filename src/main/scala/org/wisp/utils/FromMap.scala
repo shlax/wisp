9fbc43df46acb72be1219bf3f3d485a0cdd719db
peter o <slax.10101@gmail.com>
2026-03-03 05:34:12 +0100

    improve `FromMap` type class: add support for type casting with error handling and update internal logic for label handling

--- src/main/scala/org/wisp/utils/FromMap.scala
+++ src/main/scala/org/wisp/utils/FromMap.scala
@@ -27,15 +27,34 @@ trait FromMap[T] {
 object FromMap {
 
   inline given derived[T](using m: Mirror.Of[T]): FromMap[T] = {
-    lazy val labels = constValueTuple[m.MirroredElemLabels].toArray.map(_.toString)
+    lazy val labels = constValueTuple[m.MirroredElemLabels].toList.map(_.toString)
+    lazy val types = elemTypes[m.MirroredElemTypes]
     inline m match
-      case p: Mirror.ProductOf[T] => fromMapProduct(p, labels)
+      case p: Mirror.ProductOf[T] => fromMapProduct(p, labels, types)
   }
 
-  private def fromMapProduct[T](p: Mirror.ProductOf[T], labels: => Array[String]): FromMap[T] = (map: Map[String, ?]) => {
+  private inline def elemTypes[A <: Tuple]: List[(String, Any) => Any] = {
+    inline erasedValue[A] match {
+      case _: EmptyTuple => Nil
+      case _: (head *: tail) =>
+        val headElement = {
+          (nm:String, x: Any) =>
+            try {
+              x.asInstanceOf[head]
+            }catch {
+              case e:ClassCastException =>
+                throw new RuntimeException(s"Cannot cast $nm = $x", e)
+            }
+        }
+        val tailElements = elemTypes[tail]
+        headElement :: tailElements
+    }
+  }
+
+  private def fromMapProduct[T](p: Mirror.ProductOf[T], labels: => List[String], types: => List[(String, Any) => Any]): FromMap[T] = (map: Map[String, ?]) => {
     var t: Tuple = EmptyTuple
-    for (i <- labels) {
-      val v = map(i)
+    for (i <- labels.zip(types)) {
+      val v = i._2.apply(i._1, map(i._1) )
       t = t :* v
     }
     p.fromProduct(t)
