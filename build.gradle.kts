b3e192fbd24f78678a93594e97de81acd4b01417
peter o <slax.10101@gmail.com>
2026-04-09 21:43:43 +0200

    add `maven-publish` plugin, configure publishing to GitHub Packages, and update Gradle workflow to include publishing step

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ publishing{
             url = uri("https://maven.pkg.github.com/shlax/wisp")
             credentials {
                 username = "shlax"
-                password = "TOKEN"
+                password = System.getenv("TOKEN")
             }
         }
     }
