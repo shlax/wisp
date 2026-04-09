c051904f6cf1a651e63e4ca7c73765e0628d7d80
peter o <slax.10101@gmail.com>
2026-04-09 21:42:05 +0200

    add `maven-publish` plugin, configure publishing to GitHub Packages, and update Gradle workflow to include publishing step

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,30 @@
 plugins {
     scala
+    `maven-publish`
+    `java-library`
 }
 
 repositories {
     mavenCentral()
 }
 
+group = "org.wisp"
+version = "0.0.1-SNAPSHOT"
+
 dependencies {
     implementation("org.scala-lang:scala3-library_3:3.8.3")
 
     testImplementation("org.junit.jupiter:junit-jupiter:6.0.3")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
     testImplementation("com.h2database:h2:2.4.240")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 java{
     sourceCompatibility = JavaVersion.VERSION_25
     targetCompatibility = JavaVersion.VERSION_25
+
+    withSourcesJar()
 }
 
 tasks.withType<ScalaCompile>{
@@ -31,4 +38,24 @@ tasks.withType<JavaCompile>{
 
 tasks.test{
     useJUnitPlatform()
+}
+
+publishing{
+
+    publications{
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+
+    repositories{
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/shlax/wisp")
+            credentials {
+                username = "shlax"
+                password = "TOKEN"
+            }
+        }
+    }
 }
