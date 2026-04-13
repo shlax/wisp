f47671b8e52c526898d4c72e49f1a5ff7ff4399d
pochodnicky <pochodnicky@gratex.com>
2026-04-13 14:56:57 +0200

    update `build.gradle.kts` to set Java 25 compatibility and configure compiler arguments for Java and Scala tasks

--- src/main/scala/org/wisp/remote/UdpClient.scala
+++ src/main/scala/org/wisp/remote/UdpClient.scala
@@ -8,7 +8,15 @@ import org.wisp.utils.unsignedIntToBytes
 
 import java.util.zip.CRC32C
 
-class UdpClient[T](address: Option[SocketAddress] = None)(using rw:ReadWrite[T]) extends AutoCloseable {
+/**
+ * A UDP client for sending messages of type T over a datagram channel.
+ *
+ * This client serializes messages using the provided [[ReadWrite]] instance, adds CRC32C checksums
+ * for data integrity verification, and sends them to remote addresses via UDP.
+ *
+ * @param address optional socket address to bind the datagram channel to
+ */
+class UdpClient[T](address: Option[SocketAddress] = None)(using ReadWrite[T]) extends AutoCloseable {
 
   protected val channel: DatagramChannel = createDatagramChannel(address)
   protected def createDatagramChannel(adr: Option[SocketAddress]): DatagramChannel = {
@@ -17,8 +25,18 @@ class UdpClient[T](address: Option[SocketAddress] = None)(using rw:ReadWrite[T])
     dc
   }
 
-  protected def write(m: T): Array[Byte] = {
-    val buff = m.toBytes()
+  /**
+   * Serializes a message and prepends a CRC32C checksum.
+   *
+   * The resulting byte array contains:
+   * - Bytes 0-3: CRC32C checksum (4 bytes)
+   * - Bytes 4+: Serialized message data
+   *
+   * @param message the message to serialize
+   * @return byte array containing checksum and serialized message
+   */
+  protected def write(message: T): Array[Byte] = {
+    val buff = message.toBytes()
 
     val crc = new CRC32C()
     crc.update(buff)
@@ -33,14 +51,25 @@ class UdpClient[T](address: Option[SocketAddress] = None)(using rw:ReadWrite[T])
     result
   }
 
-  def send(adr: SocketAddress, m: T): Unit = {
-    val buff = write(m)
-    val r = channel.send(ByteBuffer.wrap(buff), adr)
+  /**
+   * Sends a message to the specified address.
+   *
+   * The message is serialized with a CRC32C checksum and sent via UDP.
+   *
+   * @param toAddress the destination socket address
+   * @param message   the message to send
+   */
+  def send(toAddress: SocketAddress, message: T): Unit = {
+    val buff = write(message)
+    val r = channel.send(ByteBuffer.wrap(buff), toAddress)
     if (r != buff.length) {
       throw new RuntimeException("message to long " + r + " " + buff.length)
     }
   }
 
+  /**
+   * Closes the underlying datagram channel.
+   */
   override def close(): Unit = {
     channel.close()
   }
