9b19f44019d2d56651fbc37db0569eae6b3c2d15
peter o <slax.10101@gmail.com>
2026-04-22 19:32:14 +0200

    refactor: make `Link` and `Message` covariant/contravariant to improve type safety and flexibility

--- src/main/scala/org/wisp/Link.scala
+++ src/main/scala/org/wisp/Link.scala
@@ -16,7 +16,7 @@ trait Link[-T, +R] extends Consumer[Message[T, R]]{
    */
   @targetName("send")
   def <<(v:T) : Unit = {
-    val msg = Message[T, R](t => { throw UndeliveredException(t) }, v)
+    val msg = Message[T, R]( t => { throw UndeliveredException(t) }, v)
     apply(msg)
   }
 
@@ -25,7 +25,7 @@ trait Link[-T, +R] extends Consumer[Message[T, R]]{
    */
   def call(v:T) : Future[Message[R, T]] = {
     val cf = Promise[Message[R, T]]()
-    val msg = Message[T, R](t => {
+    val msg = Message[T, R]( t => {
         if (!cf.trySuccess(t) ) throw UndeliveredException(t)
       } , v)
     apply(msg)
