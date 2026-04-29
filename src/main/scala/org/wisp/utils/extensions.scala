fb6f46d34a1863ea6dfdf89f331671dedafa1cbe
pochodnicky <pochodnicky@gratex.com>
2026-04-29 15:10:58 +0200

    doc

--- src/main/scala/org/wisp/utils/extensions.scala
+++ src/main/scala/org/wisp/utils/extensions.scala
@@ -10,7 +10,7 @@ object extensions {
   extension [T <: ActorSystem](as: T) {
 
     /**
-     * Execute `function` with `ActorSystem` given ExecutionContext from `ActorSystem`
+     * Execute `function` with `ActorSystem` given as ExecutionContext
      *
      * {{{
      * ActorSystem() || { sys =>
