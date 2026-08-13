0202c304a058723fe607f6d56a5c522304e4e605
pochodnicky <pochodnicky@gratex.com>
2026-08-13 10:46:48 +0200

    refactor: convert `RemoteAskException` to class with extractor object

--- src/main/scala/org/wisp/remote/exceptions/RemoteAskException.scala
+++ src/main/scala/org/wisp/remote/exceptions/RemoteAskException.scala
@@ -1,4 +1,12 @@
 package org.wisp.remote.exceptions
 
-case class RemoteAskException(parameter:Any)
+object RemoteAskException {
+
+  def unapply(e:RemoteAskException):Tuple1[Any] = {
+    Tuple1(e.parameter)
+  }
+
+}
+
+class RemoteAskException(val parameter:Any)
   extends UnsupportedOperationException("ask("+parameter+") is not supported")
