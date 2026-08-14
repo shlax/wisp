0e5167164cd653ede0762513b5fe3a71dc2aca94
qnx <az4789317614839174893@gmail.com>
2026-08-15 00:22:15 +0200

    Refactor: simplify `SynchronizedFlow.next` by removing `CompletableFuture` and streamlining callback execution

--- src/main/scala/org/wisp/stream/iterator/SynchronizedFlow.scala
+++ src/main/scala/org/wisp/stream/iterator/SynchronizedFlow.scala
@@ -2,7 +2,6 @@ package org.wisp.stream.iterator
 
 import org.wisp.utils.lock.withLock
 
-import java.util.concurrent.CompletableFuture
 import scala.concurrent.ExecutionContextExecutor
 import scala.util.control.NonFatal
 
@@ -11,36 +10,25 @@ trait SynchronizedFlow[T] extends StreamFlow[T], StreamLock {
   protected def nextWithLock(callback: Response[T] => Unit): Unit
 
   override def next(callback: Response[T] => Unit)(using ec : ExecutionContextExecutor) : Unit = {
-    val cf = new CompletableFuture[Response[T]]()
     ec.execute( () => {
       try {
         lock.withLock {
           nextWithLock { v =>
-            if(! cf.complete(v) ){
-              ec.reportFailure(new IllegalStateException("already completed"))
-            }
+            ec.execute( () => {
+              try {
+                callback.apply(v)
+              } catch {
+                case NonFatal(e) =>
+                  ec.reportFailure(e)
+              }
+            } )
           }
-        }
-      } catch {
-        case NonFatal(e) =>
-          if(!cf.completeExceptionally(e)){
-            ec.reportFailure(e)
-          }
-      }
-    } )
-    cf.whenCompleteAsync( (v, e) => {
-      if (e != null) {
-        ec.reportFailure(e)
-      }
-      try {
-        if(v != null){
-          callback.apply(v)
         }
       } catch {
         case NonFatal(e) =>
           ec.reportFailure(e)
       }
-    }, ec )
+    } )
   }
 
 }
