fb355e7455a38b9a9ff7d57f62bc5a510af03486
qnx <az4789317614839174893@gmail.com>
2026-08-15 00:11:32 +0200

    Refactor: change `nodes` from `def` to `val` in `SingleNodeFlow`

--- src/main/scala/org/wisp/stream/iterator/SingleNodeFlow.scala
+++ src/main/scala/org/wisp/stream/iterator/SingleNodeFlow.scala
@@ -4,7 +4,7 @@ import java.util
 
 trait SingleNodeFlow[T] extends StreamFlow[T]{
 
-  protected def nodes:util.Queue[Response[T] => Unit]
+  protected val nodes:util.Queue[Response[T] => Unit]
 
   protected def createNodes(): util.Queue[Response[T] => Unit] = {
     util.LinkedList[Response[T] => Unit]()
