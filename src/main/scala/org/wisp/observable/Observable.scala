16256724ba578fc86642959ec9223258f0ff2905
peter o <slax.10101@gmail.com>
2026-09-16 21:59:46 +0200

    feat: add functional operators (`map`, `flatMap`, `filter`, `collect`) to `Observable` for enhanced stream manipulation

--- src/main/scala/org/wisp/observable/Observable.scala
+++ src/main/scala/org/wisp/observable/Observable.scala
@@ -15,6 +15,43 @@ trait Observable[T] extends Consumer[T]{
 
   def subscribe(subscriber: T => Unit): Subscription
 
+  def map[U](f: T => U): Observable[U] = {
+    val n = Observable[U]()
+    subscribe{ (v: T) =>
+      val y = f.apply(v)
+      n.apply(y)
+    }
+    n
+  }
+
+  def flatMap[U](f: (T, Observable[U]) => Unit): Observable[U] = {
+    val n = Observable[U]()
+    subscribe { (v: T) =>
+      f.apply(v, n)
+    }
+    n
+  }
+
+  def filter(f: T => Boolean): Observable[T] = {
+    val n = Observable[T]()
+    subscribe{ (v: T) =>
+      if(f.apply(v)){
+        n.apply(v)
+      }
+    }
+    n
+  }
+
+  def collect[U](f:PartialFunction[T, U]): Observable[U] = {
+    val n = Observable[U]()
+    subscribe{ (v: T) =>
+      if(f.isDefinedAt(v)){
+        n.apply(f.apply(v))
+      }
+    }
+    n
+  }
+
 }
 
 object Observable {
