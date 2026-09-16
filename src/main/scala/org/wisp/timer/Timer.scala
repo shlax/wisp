942f04c6f12a523f5cffaf14b884fe2df73f4879
peter o <slax.10101@gmail.com>
2026-09-16 21:08:10 +0200

    feat: add overloaded scheduling methods with `AbstractObservable` for enhanced subscription handling

--- src/main/scala/org/wisp/timer/Timer.scala
+++ src/main/scala/org/wisp/timer/Timer.scala
@@ -1,5 +1,6 @@
 package org.wisp.timer
 
+import org.wisp.observable.{AbstractObservable, Observable}
 import org.wisp.Link
 
 import java.util.concurrent.{Callable, Executors, ScheduledExecutorService, ScheduledFuture, TimeUnit}
@@ -12,25 +13,92 @@ class Timer[T] extends AutoCloseable{
     Executors.newSingleThreadScheduledExecutor()
   }
 
+  /**
+   * Schedules a given value to be sent asynchronously to the specified `Link` after a specified delay.
+   * The value is computed lazily, and once the delay elapses, it is sent via the `<<` operator
+   * of the provided `Link`. The scheduled task returns the computed value.
+   */
   def schedule(link:Link[T, ?], delay:Duration, value: => T): ScheduledFuture[T] = {
     service.schedule( () => {
-      val v = value
+      val v:T = value
       link << v
       v
     }, delay.toNanos, TimeUnit.NANOSECONDS )
   }
 
+  /**
+   * Schedules a task to execute after a specified delay with a given value. The value is lazily evaluated,
+   * and the provided function is executed on an `AbstractObservable` for additional subscriptions or behavior.
+   */
+  def schedule(delay:Duration, value: => T)(fn: AbstractObservable[T] => Unit): ScheduledFuture[T] = {
+    val c = Observable[T]()
+    fn.apply(c)
+    schedule(c.apply, delay, value)
+  }
+
+  def schedule(consumer: T => Unit, delay:Duration, value: => T): ScheduledFuture[T] = {
+    service.schedule( () => {
+      val v:T = value
+      consumer.apply(v)
+      v
+    }, delay.toNanos, TimeUnit.NANOSECONDS )
+  }
+
+  /**
+   * Schedules a task to execute at a fixed rate after an initial delay.
+   * The task computes a value of type `T` using the provided `callable` function
+   * and sends it asynchronously to the specified `Link` using the `<<` operator.
+   */
   def scheduleAtFixedRate(link:Link[T, ?], initialDelay:Duration, period:Duration, callable: => T): ScheduledFuture[?] = {
     service.scheduleAtFixedRate( () => {
-      val v = callable
+      val v:T = callable
       link << v
     }, initialDelay.toNanos, period.toNanos, TimeUnit.NANOSECONDS )
   }
 
+  /**
+   * Schedules a task to execute at a fixed rate after an initial delay. The value is lazily evaluated,
+   * and the provided function is executed on an `AbstractObservable` for additional subscriptions or behavior.
+   */
+  def scheduleAtFixedRate(initialDelay:Duration, period:Duration, callable: => T)(fn: AbstractObservable[T] => Unit): ScheduledFuture[?] = {
+    val c = Observable[T]()
+    fn.apply(c)
+    scheduleAtFixedRate(c.apply, initialDelay, period, callable)
+  }
+
+  def scheduleAtFixedRate(consumer: T => Unit, initialDelay:Duration, period:Duration, callable: => T): ScheduledFuture[?] = {
+    service.scheduleAtFixedRate( () => {
+      val v:T = callable
+      consumer.apply(v)
+    }, initialDelay.toNanos, period.toNanos, TimeUnit.NANOSECONDS )
+  }
+
+  /**
+   * Schedules a task to execute repeatedly with a fixed delay between the completion of one
+   * execution and the start of the next. The task computes a value of type `T` using the provided
+   * `callable` function and sends it asynchronously to the given `Link` using the `<<` operator.
+   */
   def scheduleWithFixedDelay(link: Link[T, ?], initialDelay: Duration, delay: Duration, callable: => T): ScheduledFuture[?] = {
     service.scheduleWithFixedDelay(() => {
-      val v = callable
+      val v:T = callable
       link << v
+    }, initialDelay.toNanos, delay.toNanos, TimeUnit.NANOSECONDS)
+  }
+
+  /**
+   * Schedules a task to execute at a fixed delay an initial delay. The value is lazily evaluated,
+   * and the provided function is executed on an `AbstractObservable` for additional subscriptions or behavior.
+   */
+  def scheduleWithFixedDelay(initialDelay: Duration, delay: Duration, callable: => T)(fn: AbstractObservable[T] => Unit): ScheduledFuture[?] = {
+    val c = Observable[T]()
+    fn.apply(c)
+    scheduleWithFixedDelay(c.apply, initialDelay, delay, callable)
+  }
+
+  def scheduleWithFixedDelay(consumer: T => Unit, initialDelay: Duration, delay: Duration, callable: => T): ScheduledFuture[?] = {
+    service.scheduleWithFixedDelay(() => {
+      val v:T = callable
+      consumer.apply(v)
     }, initialDelay.toNanos, delay.toNanos, TimeUnit.NANOSECONDS)
   }
 
