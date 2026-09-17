f66da6f7a5f9941da1265fa5175e7f4460e37776
peter o <slax.10101@gmail.com>
2026-09-17 21:25:23 +0200

    refactor: simplify `register` method in `UdpRouter` by introducing `RemoteLink` for cleaner implementation

--- src/main/scala/org/wisp/remote/RemoteLink.scala
+++ src/main/scala/org/wisp/remote/RemoteLink.scala
@@ -11,11 +11,11 @@ import scala.concurrent.Future
  *
  * `ask` and `call` operations are not supported.
  */
-class RemoteLink[-T, +R](c: UdpClient[T], adr:SocketAddress) extends Link[T, R]{
+class RemoteLink[-T, +R](client: UdpClient[T], address:SocketAddress) extends Link[T, R]{
 
   override def apply(t: Message[T, R]): Unit = {
     t.process(RemoteLink.this.getClass) {
-      c.send(adr, t.value)
+      client.send(address, t.value)
     }
   }
 
